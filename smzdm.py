b6d0fb9b41bf3eaba0c3d2dd5bbf798041f898df
liruowei <379439288@qq.com>
2020-07-23 14:35:38 +0800

    c

--- smzdm.py
+++ smzdm.py
@@ -10,9 +10,8 @@ configsJson = os.environ.get('SMZDM_COOKIES')
 
 if(configsJson == "" or configsJson == None):
     configsJson = input("配置：")
-    print(configsJson)
-    configs = json.loads(configsJson)
-    print(configs)
+
+configs = json.loads(configsJson)
 
 class SmzdmSpider():
     def __init__(self):
